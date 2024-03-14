4a77b017e7a3df3935f130873ce044bd8473aa62
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-03-14 02:26:11 +0200

    theme&shadecn init

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import type { Metadata } from 'next';
 import './globals.css';
+import ThemeProvider from '@/context/ThemeProvider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -23,26 +24,26 @@ export const metadata: Metadata = {
   icons: '/assets/images/site-logo.svg'
 };
 
+
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotest.variable}`}>
-          <h1 className="font-bold ">This is a peace of text</h1>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotest.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500'
+            }
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
