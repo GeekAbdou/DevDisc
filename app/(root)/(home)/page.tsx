f06cb18cc6ce44e22753fe8e8c709772be15ca59
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-03-16 00:44:58 +0200

    question card - question post/fetch - question display

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,59 @@
-export default function Home() {
-  return <div></div>;
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import NoResult from '@/components/shared/NoResult/NoResult';
+import QuestionCard from '@/components/cards/QuestionCard';
+import { getQuestions } from '@/lib/actions/question.action';
+import Filter from './../../../components/shared/Filters/Filters';
+import { HomePageFilters } from '@/constants/filters';
+
+export default async function Home() {
+  const result = await getQuestions({});
+
+  // console.log('Home  result:', result.questions);
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href={'/ask-question'} className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[40px] rounded-lg px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+
+      {/* Card section */}
+      <div className="mt-10 flex flex-col gap-6">
+        {/*  //!Todo: looping through questions */}
+        {result.questions.length > 0 ? (
+          result.questions?.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no quesiton to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
 }
