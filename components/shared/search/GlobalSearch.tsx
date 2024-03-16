c117209f287e95170cc03f8a264247c7ca79e992
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-03-16 18:01:49 +0200

    fixing type error.

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,12 +1,12 @@
-'use client';
-import { useState } from 'react';
+'use';
+import { ChangeEvent, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 
 const GlobalSearch = () => {
   const [searchValue, setSearchValue] = useState('');
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
 
