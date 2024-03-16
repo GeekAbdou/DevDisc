cb06da6cfeab9effbaa5f8b8dae235c2d53b88b6
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-03-16 17:00:13 +0200

    build

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,17 @@
+/* eslint-disable camelcase */
 'use server';
 
 import User from '@/database/user.model';
+import { FilterQuery } from 'mongoose';
 import { connectToDatabase } from '../db/mongoose';
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetAllUsersParams,
+  UpdateUserParams,
+} from './shared.types';
+import { revalidatePath } from 'next/cache';
+import Question from '@/database/question.model';
 
 export const getUserById = async (params: any) => {
   try {
@@ -12,6 +22,113 @@ export const getUserById = async (params: any) => {
     const user = await User.findOne({ clerkId: userId });
 
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+// create a user in the database
+export const createUser = async (userData: CreateUserParams) => {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateUser = async (params: UpdateUserParams) => {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+export const deleteUser = async (params: DeleteUserParams) => {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // delete user Questions
+    await Question.deleteMany({ author: user._id });
+
+    // todo: delete user answers, comments etc
+
+    // delete user
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getAllUsers = async (params: GetAllUsersParams) => {
+  try {
+    connectToDatabase();
+
+    const { page = 1, pageSize = 10, searchQuery, filter } = params;
+
+    // pagination:
+
+    const skipAmount = (page - 1) * pageSize;
+
+    const query: FilterQuery<typeof User> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, 'i') } },
+        { username: { $regex: new RegExp(searchQuery, 'i') } },
+      ];
+    }
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case 'new_users':
+        sortOptions = { joinAt: -1 };
+        break;
+      case 'old_users':
+        sortOptions = { joinAt: 1 };
+        break;
+      case 'top_contributors':
+        sortOptions = { reputation: -1 };
+
+        break;
+
+      default:
+        break;
+    }
+
+    const users = await User.find(query)
+      .skip(skipAmount)
+      .limit(pageSize)
+      .sort(sortOptions);
+
+    const totalUsers = await User.countDocuments(query);
+
+    const isNext = totalUsers > skipAmount + users.length;
+
+    return { users, isNext };
   } catch (error) {
     console.log(error);
     throw error;
