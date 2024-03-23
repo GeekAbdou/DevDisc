5738801f8d20b3dbf009f3605c4bfc1df035510b
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-03-24 01:00:17 +0200

    edit profile page & top qustions sidebar

--- components/shared/RightSidebar/RenderTag.tsx
+++ components/shared/RightSidebar/RenderTag.tsx
@@ -1,5 +1,3 @@
-// interface example
-
 import Link from 'next/link';
 import { Badge } from '@/components/ui/badge';
 
@@ -18,7 +16,7 @@ const RenderTag = ({
 }: RenderTagProps) => {
   return (
     <Link href={`/tags/${_id}`} className="flex justify-between gap-3">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500  rounded-md border-none px-4 py-2 uppercase">
         {name}
       </Badge>
       {showCount && (
